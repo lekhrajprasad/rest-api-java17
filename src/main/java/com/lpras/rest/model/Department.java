b8056e7facbb77c26c2a2f7a68ea03423c5dcefb
lekhrajprasad <Welcome@123>
2022-12-24 09:43:08 +0530

    commti 2

---
+++ src/main/java/com/lpras/rest/model/Department.java
@@ -0,0 +1,23 @@
+package com.lpras.rest.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+// Class
+public class Department {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long departmentId;
+	private String departmentName;
+	private String departmentAddress;
+	private String departmentCode;
+}
---
+++ src/main/java/com/lpras/rest/repository/DepartmentRepository.java
@@ -0,0 +1,8 @@
+package com.lpras.rest.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends CrudRepository<Department, Long> {
+
+}
